772ee762b68f5190e0e430280be991b90babd801
TheNoobDevs <thenoobdevs2024@gmail.com>
2024-07-09 06:36:52 +0100

    Add packages, and API levels

--- device.mk
+++ device.mk
@@ -6,3 +6,26 @@
 #
 
 LOCAL_PATH := device/samsung/gta9
+
+# API levels
+PRODUCT_SHIPPING_API_LEVEL := 32
+PRODUCT_TARGET_VNDK_VERSION := 32
+
+# Dynamic
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd 
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Additional Packages
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
